6b4ad8e1b2a5bdcb70ddd1fa6573850910843dd1
S.S. Syntax-Corp <asgu1520@gmail.com>
2026-01-31 20:00:19 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const token = urlParams.get('token');
-if (token) {
-    localStorage.setItem('discord_token', token);
-    window.history.replaceState({}, '', '/');  // Clean up URL
-}
